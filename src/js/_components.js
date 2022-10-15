fe91303eb0b36fcdccb3b9e32f7b2aa90aac72de
Timofey Krivenkov <drake9100@mail.ru>
2022-10-15 15:27:28 +0300

    UPD: Список хитов продаж

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,6 +2,7 @@ import { initCustomSelect } from "./components/init-custom-select";
 import { initSubmenu } from "./components/init-submenu";
 import { getHeaderHeight } from './functions/header-height';
 import { initHeroSlider } from './components/init-hero-slider';
+import { initTabs } from './components/init-tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
   initSubmenu();
@@ -9,5 +10,6 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initCustomSelect();
     initHeroSlider();
+    initTabs();
   });
 });
---
+++ src/js/components/init-tabs.js
@@ -0,0 +1,7 @@
+import GraphTabs from 'graph-tabs';
+
+function initTabs() {
+  const tabs = new GraphTabs('products');
+}
+
+export {initTabs}
