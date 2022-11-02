cc46bc6e6103cc48397e0ac5c6139b0973e2ae93
Timofey Krivenkov <drake9100@mail.ru>
2022-11-02 16:12:37 +0300

    RFC: Ошибки скрипта в консоли

--- src/js/components/init-product-tabs.js
+++ src/js/components/init-product-tabs.js
@@ -1,6 +1,10 @@
 import GraphTabs from 'graph-tabs';
 
 function initProductTabs() {
+  const prodTabEl = document.querySelector('[data-tabs=productTabs]');
+  if(!prodTabEl) {
+    return;
+  }
   const productTabs = new GraphTabs('productTabs');
 }
 
