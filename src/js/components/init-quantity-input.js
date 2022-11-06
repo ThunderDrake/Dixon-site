362df0b40125497229e800e2169bfb01389537e1
Timofey Krivenkov <drake9100@mail.ru>
2022-11-06 22:35:19 +0300

    UPD: Страница вакансий

--- src/js/components/init-quantity-input.js
+++ src/js/components/init-quantity-input.js
@@ -9,7 +9,6 @@ function initQuantityInput() {
     let quantityNumber = el.closest('.quantity').querySelector('.quantity__value');
     let quantityInput = el.closest('.quantity').querySelector('.quantity__input');
 
-    console.log(quantityInput.value = 5);
     el.addEventListener('click', (e) => {
       if(e.target.classList.contains('quantity__button--plus')) {
 
--- src/js/components/init-vacancy-select.js
+++ src/js/components/init-vacancy-select.js
@@ -2,6 +2,7 @@ import Choices from 'choices.js';
 
 function initVacancySelect() {
   const vacancySelects = document.querySelectorAll('.vacancy__select');
+  const vacancyInfo = document.querySelectorAll('.vacancy__info');
 
   if(!vacancySelects.length) {
     return
@@ -11,6 +12,12 @@ function initVacancySelect() {
       searchEnabled: false,
       itemSelectText: '',
     });
+
+    el.addEventListener('choice', function(event){
+      vacancyInfo.forEach(el=>{el.classList.remove('active')});
+      [...vacancyInfo].find(item=>item.dataset.value === event.detail.choice.value).classList.add('active');
+    },
+    false)
   })
 }
 
