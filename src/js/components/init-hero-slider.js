049f643dda5285baf33a4c96f9b481a160d2864d
Timofey Krivenkov <drake9100@mail.ru>
2022-11-25 03:56:43 +0300

    ADD: Второй тип слайдера на первом экране

--- src/js/components/init-hero-slider.js
+++ src/js/components/init-hero-slider.js
@@ -1,14 +1,17 @@
-import Swiper, { Pagination } from 'swiper';
+import Swiper, { Pagination, Autoplay } from 'swiper';
 
 function initHeroSlider() {
   const heroSlider = document.querySelector('.hero-slider')
-  Swiper.use([Pagination]);
+  Swiper.use([Pagination, Autoplay]);
   const swiper = new Swiper(heroSlider, {
     slidesPerView: 1,
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
     },
+    autoplay: {
+      delay: 5000
+    }
   });
 }
 
